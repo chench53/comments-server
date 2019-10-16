15f4b3dead770846bbb7c0d6aec889c64df6387c
chench53 <489386728@qq.com>
2019-10-16 22:41:48 +0800

    add test case for creator

--- comments/tests.py
+++ comments/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from comments.models import Creator, Comment
+
+class CommentTestCase(TestCase):
+    def setUp(self):
+        creator_data = {
+            'fullname': 'mom',
+            'profile_picture_url': 'http://cdn.moastro.cn/user_avatar/wx88acc94e3b3d2bce/oFh8LwWJebXNkReq1f9PO4RsLkp0.jpg'
+        }
+        self.test_creator = Creator.objects.create(**creator_data)
+
+    def test_have_creator(self):
+        mom = Creator.objects.get(fullname='mom')
+        self.assertEqual(mom.profile_picture_url, 'http://cdn.moastro.cn/user_avatar/wx88acc94e3b3d2bce/oFh8LwWJebXNkReq1f9PO4RsLkp0.jpg')
